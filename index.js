7956393aa157cda77007d113bd067dc00ac9a4ac
Brendan J Beirne <brendan.beirne@gmail.com>
2020-11-08 01:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+// write yourf code below!
+var happyHolidays = "Happy holidays!"
+
+function happyHolidays {
+  return "Happy holidays!"
+}
+var name
+function happyHolidaysTo {
+  return `Happy holidays, ${name}!`
+}
