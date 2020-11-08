d6a13f6fb8a275b786884adbd6552e402d4c3a60
Brendan J Beirne <brendan.beirne@gmail.com>
2020-11-08 02:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,15 @@
 function happyHolidays() {
   return "Happy holidays!";
 }
-happyHolidays();
-
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo();
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
-
-holidayCountdown();
 
 function holidayCountdown(){
   return `It's ${days} days until ${holiday}!`;
 }
-}
+
